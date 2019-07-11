5a175432b602d540c918b3c52296bc6749e131c4
Allen Hai <allenhai03@gmail.com>
2019-07-11 12:23:34 -0500

    init

---
+++ www/pages/about.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default function Index() {
+  return <>
+    <h1>about</h1>
+    <Link href="/">index</Link>
+  </>
+}
---
+++ www/pages/index.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default function Index() {
+  return <>
+    <h1>index</h1>
+    <Link href="/about">about</Link>
+  </>
+}
